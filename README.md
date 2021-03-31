7ac6faec8c824655a7ca09d66216c419780c1546
jaedeokhan <dgd03023@cu.ac.kr>
2021-04-01 00:38:06 +0900

    add 0331

--- README.md
+++ README.md
@@ -18,5 +18,37 @@
 ## Codeup-100
 * [1 ~ 100.py](https://github.com/jaedeokhan/start-coding-test-pratice/tree/main/codeup-100)
 
+## Python 함수
+
+#### zip 
+* zip()은 동일한 개수로 이루어진 자료형을 묶어주는 역할을 하는 함수이다.
+* https://docs.python.org/ko/3/library/functions.html#zip
+   * zip(*iterablesa)
+
+
+```python
+number = [1, 2, 3, 4]
+name = ['han', 'kim', 'lee', 'hong']
+
+number_name = list(zip (number, name))
+print(number_name)
+
+# 결과 : [ (1, 'han'), (2, 'kim'), (3, 'lee'), (4, 'hong') ] 
+```
+
+#### eval
+* 매개변수로 받은 식을 문자열로 받아서  파이썬 코드로 실행할 때 사용합니다.
+* https://docs.python.org/ko/3/library/functions.html#eval
+
+```python
+x = 1
+eval('x + 1')
+
+# 결과 : 2
+```
+
+#### join
+* str.join(iterable)
+* https://docs.python.org/3/library/stdtypes.html#str.join
 
 
--- algorithm_programming_tutorial/02_bubble_sort.py
+++ algorithm_programming_tutorial/02_bubble_sort.py
@@ -3,17 +3,17 @@
 # 옆에 있는 값과 비교하여 더 작은 값을 반복적으로 앞으로 보내는 정렬 방법
 # 시간 복잡도 O(N * N)
 
-data = [10, 1, 3, 5, 7, 8, 4, 9, 2, 6]
+data = list(map(int, input().split()))
 
-for i in range(len(data)): # 0~9
+for i in range(len(data)): # 10 => 0 ~ 9
     for j in range(len(data) - i - 1):
-        # 10 - 0 - 1 => 9 => 0~8
-        # 10 - 1 - 1 => 8 => 0~7
-        if data[j] > data[j+1]:
-            temp = data[j]
-            data[j] = data[j+1]
-            data[j+1] = temp
-
-print(data)
-            
+    # 10 - 0 - 1 => 9 => 0 ~ 8
+    # 10 - 1 - 1 => 8 => 0 ~ 7
+    # 10 - 2 - 1 => 7 => 0 ~ 6
+    # ...
+    # 10 - 9 - 1 => 0 => 0
+        if(data[j] > data[j+1]):
+            data[j], data[j+1] = data[j+1], data[j]
         
+for i in range(len(data)):
+	print(data[i], end=' ')
