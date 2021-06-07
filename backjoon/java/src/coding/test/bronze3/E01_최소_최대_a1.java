43f593851e58d2df346cbc0c3253837c557cc24e
jaedeokhan <dgd03023@cu.ac.kr>
2021-06-07 11:53:14 +0900

    add [java][bronze3][10818번] 최소,최대

---
+++ backjoon/java/src/coding/test/bronze3/E01_최소_최대_a1.java
@@ -0,0 +1,31 @@
+package coding.test.bronze3;
+
+import java.util.Scanner;
+
+public class E01_최소_최대 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] num_array = new int[num];
+        int largest = -2148000;
+        int smallest = 2148000;
+
+        for (int i = 0; i < num; i++) {
+            num_array[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < num; i++) {
+            if (num_array[i] > largest){
+                largest = num_array[i];
+            }
+
+            if (num_array[i] < smallest) {
+                smallest = num_array[i];
+            }
+        }
+
+        System.out.println(smallest + " " + largest);
+
+    }
+}
---
+++ backjoon/java/src/coding/test/bronze3/E01_최소_최대_a2.java
@@ -0,0 +1,2 @@
+package coding.test.bronze3;public class E01_최소_최대_a2 {
+}
