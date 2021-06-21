ae9918f36b45fdcf9ddb3e7d461346c8322f0895
jaedeokhan <dgd03023@cu.ac.kr>
2021-06-21 17:11:48 +0900

    [java] 수정 Section01 11_문자열 압축 13행 for문 조건문 수정

--- java/src/coding/section01/E11_문자열_압축/Main_A.java
+++ java/src/coding/section01/E11_문자열_압축/Main_A.java
@@ -10,7 +10,7 @@ public class Main_A {
         s = s + " ";
         int count = 0;
 
-        for (int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length()-1; i++){
             if (s.charAt(i) == s.charAt(i + 1)){
                 count++;
             } else {
