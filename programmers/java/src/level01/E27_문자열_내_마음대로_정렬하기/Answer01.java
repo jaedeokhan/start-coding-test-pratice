9b87cee0c2125c99de6dcfc2bc27912e5367167e
jaedeokhan <dgd03023@cu.ac.kr>
2021-07-11 00:09:24 +0900

    add [java] Level01 27_문자열 내 마음대로 정렬하기 - Comparator()를 사용해서 String s에 n번째 인덱스를 기준으로 정렬을 해서 해결    - 그러나, int n = 2이고, "abcd", "abce" 와 같이 동일한 'c' 값이    있으면 o1.compareTo(o2) 사전식으로 정렬해서 해결

--- programmers/java/src/level01/E27_문자열_내_마음대로_정렬하기/Answer01.java
+++ programmers/java/src/level01/E27_문자열_내_마음대로_정렬하기/Answer01.java
@@ -1,25 +1,33 @@
-package level01.E26_문자열_내림차순으로_배치하기;
+package level01.E27_문자열_내_마음대로_정렬하기;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.Comparator;
 
 public class Answer01 {
 
-    public String solution(String s) {
+    public String[] solution(String[] strings, int n) {
 
-        char[] sValues = s.toCharArray();
-        Arrays.sort(sValues);
+        Arrays.sort(strings, new Comparator<String>(){
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.charAt(n) > o2.charAt(n)) return 1;
+                else if (o1.charAt(n) == o2.charAt(n)) return o1.compareTo(o2);
+                else return -1;
+            }
+        });
 
-        return new StringBuilder(new String(sValues)).reverse().toString();
+        return strings;
     }
 
     public static void main(String[] args) {
 
         Answer01 T = new Answer01();
-        String s = "Zbcdefg";
+        String[][] strings = { {"sun", "bed", "car"}, {"abce", "abcd", "cdx"}};
+        int[] n = {1, 2};
 
-        System.out.println(T.solution(s));
+        for (int i = 0; i < strings.length; i++){
+            System.out.println(Arrays.toString(T.solution(strings[i], n[i])));
+        }
     }
 
 }
