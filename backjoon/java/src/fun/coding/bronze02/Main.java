5c324a51e5608afd434546b21628ad67d1c97104
jaedeokHan <hjaedeok@gmail.com>
2021-08-16 11:54:59 +0900

    [add] - Java bronze2 - 숫자의 합 - 큰 입력 정수값이 있어서 BufferedReader로 받고 charAt(index)로 읽어가며 해결

---
+++ backjoon/java/src/fun/coding/bronze02/Main.java
@@ -0,0 +1,24 @@
+package fun.coding.bronze02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    // bronze2 - 숫자의 합[https://www.acmicpc.net/problem/11720]
+    // N개의 숫자를 입력받아서 모두 합해서 처리
+    // 합계 값이 int형보다 클 수 있으니 long으로 해결
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String defaultSplitValue = br.readLine();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += (defaultSplitValue.charAt(i) - 48);
+        }
+
+        System.out.println(sum);
+    }
+}
