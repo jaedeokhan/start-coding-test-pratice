182f5837920d0d5629d671f6f03551911928937d
jaedeokHan <hjaedeok@gmail.com>
2021-08-17 01:07:39 +0900

    [add] - Java bronze1, 1157번, 단어 공부 - 처음에 대소문자를 구별하지 않는 것을 못보고 잘못 접근했음....이런 - 다시 대소문자를 구별하지 않는 것을 보고 String의 메소드인 toUpperCase()를 통해서 대문자화 했음 - for문을 i부터 word.length()까지 돌면서 해당 문자열을 각각 문자를 'A'로 빼면서 0 ~ 26 인덱스 값에 증감해서 카운팅 - getMaxValueIndex(int[] answer)    - 최댓값의 인덱스를 반환 메서드 구현 - checkDuplicateMaxValue(int maxIndex, int[] answer)    - 최댓값이 중복되는 체크하는 메서드 구현

---
+++ backjoon/java/src/fun/coding/bronze01/Main.java
@@ -0,0 +1,69 @@
+package fun.coding.bronze01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    // bronze1 - 1157번 - 단어 공부[https://www.acmicpc.net/problem/1157]
+    // 문제 - 알파벳 대소문자로 된 단어가 주어지면 이 단어에서 가장 많이 사용되는 알파벳을 찾아내는 프로그램
+    // 주의 - 대소문자 구별 x
+
+    // 최댓값의 인덱스를 구하는 함수
+    public int getMaxValueIndex(int[] answer){
+        int max = Integer.MIN_VALUE;
+        int index = 0;
+
+        for (int i = 0; i < answer.length; i++){
+            if (answer[i] > max){
+                max = answer[i];
+                index = i;
+            }
+        }
+
+        return index;
+    }
+
+    // 최댓값이 중복되는지 체크하는 함수
+    public boolean checkDuplicateMaxValue(int maxIndex, int[] answer){
+        int duplicateCount = 0;
+
+        for (int i = 0; i < answer.length; i++){
+            if (answer[maxIndex] == answer[i]) duplicateCount++;
+        }
+
+        // 최댓값(maxIndex) 이외에 동일한 값이 있으면 카운트는 2개 이상이기에 true
+        if (duplicateCount > 1) return true;
+
+        return false;
+    }
+
+    public char solution(String word){
+
+        int[] answer = new int[26]; // A-Z를 담을 26개의 공간 할당
+        word = word.toUpperCase();  // 모두 대문자화
+
+        for (int i = 0; i < word.length(); i++){
+            char alphabet = word.charAt(i);
+            answer[alphabet - 'A'] += 1;   // Ex) A가  65이면  => (65 - 65) == 0는 0이다.
+        }
+
+        // 최댓값의 인덱스 구하는 메서드
+        int maxIndex = getMaxValueIndex(answer);
+
+        // 최댓값이 중복되는지 구하는 메서드
+        if (checkDuplicateMaxValue(maxIndex, answer)) return '?'; // 중복되는 최댓값이 있으면 ?를 리턴
+
+        // 여기까지 오면 최댓값이 중복되지 않기에 maxIndex + 65를 하고
+        // (char) 명시적 형변환하면 해당 대문자 출력
+        return (char) (maxIndex + 65);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Main T = new Main();
+        String word = br.readLine();
+        System.out.println(T.solution(word));
+    }
+}
